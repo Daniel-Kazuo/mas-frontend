01549d68cd77d6c2db70d19d5c0d16bb0b15c6c2
Daniel-Kazuo <daniel.kazuo@gmail.com>
2021-11-18 14:32:26 -0300

    Frontend 18-11-2021

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import {BrowserRouter as Router} from 'react-router-dom'
 import Routes from './routes';
 import GlobalStyle from './Styles/global';
+import { AuthProvider } from "./hooks/Auth";
 function App() {
 
   return (
+    <AuthProvider>
       <Router>
         <Routes />
         <GlobalStyle />
       </Router>
+      </AuthProvider>
   );
 }
 
